84cca980de3f2d26e0b496a42d310a52ef66ca9c
69070020 <69070020@kmitl.ac.th>
2026-07-03 15:57:26 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,8 +1,8 @@
 """การตรวจสอบบัตรนักศึกษา"""
 def main():
     """Function for calculate"""
-    id = input()
-    if id[2] == "1" and id[3] == "6":
+    number = input()
+    if number[2] == "1" and number[3] == "6":
         print("yes")
     else:
         print("no")
