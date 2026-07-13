2eb162b355ea7b3109ea577d9387a1fcf0df278b
it69070020 <69070020@kmitl.ac.th>
2026-07-13 21:30:04 +0700

    Update Colors.py

--- 3011/Colors.py
+++ 3011/Colors.py
@@ -11,6 +11,12 @@ def main():
         print("Violet")
     elif first_color == "Yellow" and second_color == "Blue":
         print("Green")
+    elif first_color == "Red" and second_color == "Red":
+        print("Red")
+    elif first_color == "Yellow" and second_color == "Yellow":
+        print("Yellow")
+    elif first_color == "Blue" and second_color == "Blue":
+        print("Blue")
     else:
         print("Error")
 
