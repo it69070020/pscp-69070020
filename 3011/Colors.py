4c70c7b7f1c616f0e024d0bee80dc5d197741473
it69070020 <69070020@kmitl.ac.th>
2026-07-13 21:38:17 +0700

    Update Colors.py

--- 3011/Colors.py
+++ 3011/Colors.py
@@ -2,20 +2,22 @@
 def main():
     """Primary color"""
 
-    first_color = str(input())
-    second_color = str(input())
+    first_color = input().strip()
+    second_color = input().strip()
+    new_first_color = first_color.capitalize()
+    new_second_color = second_color.capitalize()
 
-    if first_color == "Red" and second_color == "Yellow":
+    if new_first_color == "Red" and new_second_color == "Yellow":
         print("Orange")
-    elif first_color == "Red" and second_color == "Blue":
+    elif new_first_color == "Red" and new_second_color == "Blue":
         print("Violet")
-    elif first_color == "Yellow" and second_color == "Blue":
+    elif new_first_color == "Yellow" and new_second_color == "Blue":
         print("Green")
-    elif first_color == "Red" and second_color == "Red":
+    elif new_first_color == "Red" and new_second_color == "Red":
         print("Red")
-    elif first_color == "Yellow" and second_color == "Yellow":
+    elif new_first_color == "Yellow" and new_second_color == "Yellow":
         print("Yellow")
-    elif first_color == "Blue" and second_color == "Blue":
+    elif new_first_color == "Blue" and new_second_color == "Blue":
         print("Blue")
     else:
         print("Error")
